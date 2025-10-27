13e76d4f6a6643d5a6d1bddae98a90eb2502a1c5
Murad Karimov <muradkarimov07@gmail.com>
2025-10-27 17:35:55 +0300

    created UserRepositories

---
+++ src/main/java/com/murad/repositories/UserRepositories.java
@@ -0,0 +1,9 @@
+package com.murad.repositories;
+
+import com.murad.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepositories extends JpaRepository<User, Long> {
+}
